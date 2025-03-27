b02a91e9aa0f9d7388f0d1c8fad47eb162f6c82b
Dennis Lopes Silva <dennislopes@gmail.com>
2025-03-27 11:48:16 -0300

    Adding nmap scan code

--- app.py
+++ app.py
@@ -178,4 +178,4 @@ def submit():
 # Resto das rotas para as outras opções
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=5000)
